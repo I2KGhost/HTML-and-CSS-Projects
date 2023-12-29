9585f604f34ccd87a0fe74e2c9d1250bd456ad10
Ivaylo Kolev <i2k.enigma@gmail.com>
2023-12-29 23:43:03 +0000

    new and old

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,7 +1,7 @@
 alert("Red and Green Color");
 // Function script
 function My_First_Function() {
-  var String = "Kiss me, I'm Irish";
+  var String = "Kiss me, I'm Bulgarian";
   var result = String.fontcolor("green");
-  document.getElementById("Irish").innerHTML = result;
+  document.getElementById("Bulgarian").innerHTML = result;
 }
